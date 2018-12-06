535c4e8fbdd457217bb4881108fe5c42505c401b
Flavio Oliveira <flavio.m@samsung.com>
2018-12-06 17:29:03 -0200

    Creating components using classes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import  React from 'react';
+import  ReactDOM from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component {
+
+  constructor (props) {
+    super(props);
+
+    // THIS IS THE ONLY TIME we do direct assignment
+    // to this.state
+    this.state = { lat: null };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        // we called set state!!!
+        this.setState({lat: position.coords.latitude});
+
+        // WE DID NOT!!!
+        // this.state.lat = position.coords.latitude;
+      },
+      (err) => console.log(err)
+    );
+
+  }
+
+  // React says we have the define render!!
+  render () {
+    return <div>Latitude: {this.state.lat}</div>; 
+  }
+}
+
+ReactDOM.render(
+  <App/>, document.querySelector('#root')
+);
